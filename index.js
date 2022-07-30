d3ff1c58fc3fd8b97ea35b8787aa99065cbe7cfb
Sviatoslav <loomyapsvr@gmail.com>
2022-07-30 16:41:09 -0400

    Version with working /rate /subscribe and swagger

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const express = require('express');
+const fs = require('fs');
+
+const bodyParser = require('body-parser');
+
+const lowdb = require('lowdb');
+
+const FileS = require("lowdb/adapters/FileSync");
+
+const ad = new FileS("db.json");
+const db = lowdb(ad);
+
+
+
+
+let newData={
+    "email":"1@gmail.com"
+};
+
+
+
+
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const { listenerCount } = require('events');
+swaggerDocument = require('./swagger.json');
 
 const Client = require('coinbase').Client;
 const client = new Client({
@@ -10,39 +37,41 @@ const client = new Client({
 
 currencyCode = 'UAH'
 
-// const currencies = [
-//     { id: "BTC", name: "bitcoin"},
-//     {id: 2, name: "ethereum"},
-//     {id: 3, name: "litecoin"},
-// ];
-
-app.get('/', (req,res) => {
-    res.send('Hello, world!!!');
-});
-
-// app.get('/api/rate/:id', (req,res) => {
-//     const currency = currencies.find(c => c.id === parseInt(req.params.id));
-    
-
-//     if(!currency) res.status(404).send("Wrong ID");
-//     res.send(currency);
-// });
-
-app.get('/api/rate/', (req,res) => {
+app.get('/api/rate', (req,res) => {
         client.getSpotPrice({"currency": currencyCode}, function(err, price) {
-        res.send('Current bitcoin price in ' + currencyCode + ': ' +  price.data.amount);
-        res.sendStatus(200);
-        
-        console.log('Current bitcoin price in ' + currencyCode + ': ' +  price.data.amount);
+            res.json({"BTC-UAH": price.data.amount});
         });
     
 });
 
+app.post('/api/subscribe', (req,res) => {
+    const email = req.body;
+
+    const check = req.app.db.get("emails").find(email).value();
+  
+     if (!check){
+        
+        app.db.get("emails").push(req.body).write();
+        res.send(200);
+    }
+    else {
+        res.send(409);
+    }
+   
+});
 
 
 
 //Server part
 //PORT 
 const port = process.env.PORT || 3000
+app.db = db;
 
+
+app.use(
+    '/api',
+    swaggerUI.serve, 
+    swaggerUI.setup(swaggerDocument)
+  );
+  
 app.listen(port,() => console.log(`Listening on port: ${port}...`));
